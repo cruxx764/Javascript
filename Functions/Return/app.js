ed80202dda84aa3af4196e2631057b1bbd98af82
harrison-shaw <harrisonshaw764@gmail.com>
2021-08-14 22:34:03 +1000

    updating functions dir

--- Functions/Return/app.js
+++ Functions/Return/app.js
@@ -1,58 +1,58 @@
-// function add(x,y){
-//     return x + y;
-// }
-// console.log(add(2,2));
+function add(x,y){
+    return x + y;
+}
+console.log(add(2,2));
 
-// function check(x,y){
-//     if (typeof x !=='number'|| typeof y !== 'number'){
-//     return false;
-//     }
-//     return x + y;
-// }
+function check(x,y){
+    if (typeof x !=='number'|| typeof y !== 'number'){
+    return false;
+    }
+    return x + y;
+}
 
-// console.log(check(2,6));
+console.log(check(2,6));
 
-// function lastElement(array){
-//     if(array.length !== 0){
-//       return array[array.length - 1] 
-//     } return null
-// }
+function lastElement(array){
+    if(array.length !== 0){
+      return array[array.length - 1] 
+    } return null
+}
 
 
-// // console.log(lastElement([]))
+// console.log(lastElement([]))
 
 // // Solution 1
-// function capitalize(word){
-//     return `${word[0].toUpperCase()}`+ word.substring(1)
-// }
+function capitalize(word){
+    return `${word[0].toUpperCase()}`+ word.substring(1)
+}
 
-// console.log(capitalize('eggplant'))
+console.log(capitalize('eggplant'))
 
 // //Solution 2
-// function capitalize(word){
-//     return word.charAt(0).toUpperCase() + word.slice(1);
-// }
+function capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
 
-// console.log(capitalize('eggplant'))
+console.log(capitalize('eggplant'))
 
 
 // Solution 1
-// function sumArray(array) {
-//     let arraValue = 0
-//     for (i of array) {
-//         arraValue += array[i];
-//     } return arraValue
-// }
+function sumArray(array) {
+    let arraValue = 0
+    for (i of array) {
+        arraValue += array[i];
+    } return arraValue
+}
 
 
 // Solution 2
-// function sumArray(array){
-//     return array.reduce(function(acc,val){
-//         return acc + val;
-//     })
-// }
+function sumArray(array){
+    return array.reduce(function(acc,val){
+        return acc + val;
+    })
+}
 
-// console.log(sumArray([1,2,2]));
+console.log(sumArray([1,2,2]));
 
 function returnDay(day){
     let days = [ null ,'Monday', 'Tuesday','Wednesday', 'Thursday','Friday','Saturday','Sunday']
