221dc1dce585b9ae179297ef438d2c210178e230
harrison-shaw <harrisonshaw764@gmail.com>
2021-08-15 14:38:07 +1000

    updating with definingMethods

---
+++ Functions/levelUpFunctions/Scope/app.js
@@ -0,0 +1,15 @@
+// function collectEggs(){
+//     let total Eggs = 6;
+// }
+
+// collectEggs():
+// console.log(totalEggs); 
+
+
+let bird = 'Scarlet Macaw';
+function birdWatch(){
+    let bird = 'great blue heron';
+    console.log(bird)
+}
+
+birdWatch()
---
+++ Functions/levelUpFunctions/functionExpressions/app.js
@@ -0,0 +1,15 @@
+function add(x,y) {
+    return x+y;
+}
+
+// functino has no name - the variable is named in this case
+// const add = function (x,7) {
+//     return x + y;
+// }
+
+const square = function (arrg) {
+    return arrg * arrg; 
+}
+
+console.log(square(5));
+
